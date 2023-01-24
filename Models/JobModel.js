bc476e210a19949f65d91a6d2c6972ceb1bd7f70
Janhaviprabhu <januprabhu26@gmail.com>
2023-01-24 13:12:50 +0530

    job list and admin crud achieved

--- Models/JobModel.js
+++ Models/JobModel.js
@@ -2,16 +2,11 @@ const mongoose = require("mongoose");
 
 const jobSchema = mongoose.Schema({
     company: { type: String, required: true },
-    postedAt: { type: Date, default: Date.now() },
-    city: { type: String, required: true },
     location: { type: String, required: true },
-    role: { type: String, required: true },
-    level: { type: String, required: true },
     contract: { type: String, required: true },
     position: { type: String, required: true },
-    language: { type: String, required: true },
 });
 
-const JobModel = mongoose.model("job", jobSchema);
+const JobModel = mongoose.model("jobs", jobSchema);
 
 module.exports = JobModel;
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const mongoose = require('mongoose');
 const { UserModel } = require("./Models/UserModel");
+const JobModel = require("./Models/JobModel");
 
 
 const app = express()
@@ -59,6 +60,64 @@ app.post("/login", async (req, res) => {
         }
     }
 })
+
+//admin route
+app.get('/admin',(req,res)=>{
+    res.send('admin page ')
+})
+
+app.post('/admin/jobPost',async(req,res)=>{
+    const { company, location, contract, position} =
+        req.body;
+
+    const job_lists = new JobModel({
+        company,
+        location,
+        contract,
+        position,
+    });
+    await job_lists.save();
+    res.send({ job_lists });
+
+})
+app.delete('/admin/delete/:id',async(req,res)=>{
+    
+    const { id } = req.params;
+    try {
+        await JobModel.findByIdAndDelete({ _id: id });
+        res.send({ "message": "Job Deleted Successfully!!" })
+    } catch (error) {
+        res.status(400).send({ "message": "Something Went Wrong" });
+    }
+})
+
+app.patch('/admin/edit/:id', async (req, res) => {
+    const payload = req.body;
+    const { id } = req.params;
+    if (payload === '') res.status(400).send({ "message": "Need to Login First" })
+    else {
+        try {
+            await JobModel.findByIdAndUpdate({ _id: id }, payload);
+            res.send({ "message": "Job Updated Successfully!!" });
+        } catch (error) {
+            res.status(400).send({ "message": "Something Went Wrong" });
+        }
+    }
+});
+app.get('/jobList',async(req,res)=>{
+    let jobs=[]
+   const data=await JobModel.find()
+   jobs.push({data})
+   res.send(jobs)
+})
+
+app.get('/applied',(req,res)=>{
+    res.send()
+})
+
+
+
+
 
 mongoose.set('strictQuery', true)
 mongoose.connect('mongodb+srv://janhavi:12345@cluster0.esiytt3.mongodb.net/job-app?retryWrites=true&w=majority').then(() => {
